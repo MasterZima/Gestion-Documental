c6f37e2f63dfb79999e81ee393a84033c946fed8
MasterZima <juangarzon@zimainversiones.com>
2020-02-24 15:45:19 -0500

    Adicionar y actualizar tablas BD

--- resources/views/admin/permiso/crear.blade.php
+++ resources/views/admin/permiso/crear.blade.php
@@ -5,7 +5,7 @@
 @section('contenido')
     <div class="row">
         <div class="col-lg-12">
-            <div class="card card-warning">
+            <div class="card card-danger">
                 <div class="card-header">
                     <h3 class="card-title">Crear Permisos</h3>
                 </div>
